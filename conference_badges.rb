67f8f4d8a58ebb85522808b9f5fd16a48c6b8fb1
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-12-31 11:06:29 -0500

    commit tree 5c072bb0a9dcfbd48df8526609faaeca97522c05

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator names
+    names.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms names
+    names.map.with_index(1) do |name, room_num|
+        "Hello, #{name}! You'll be assigned to room #{room_num}!"
+    end
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |badge|
+      puts badge
+    end
+end
